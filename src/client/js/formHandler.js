2bda9b6e93cdf05ab4e80a9d33f38e744dc9aee6
dahhnym <danimkim.dev@gmail.com>
2023-07-02 13:50:45 +0900

    ⬆️ upgrade dependencies and make basic setup

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,24 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+  // check what text was put into the form field
+  let formText = document.getElementById("name").value;
+  Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+  console.log("::: Form Submitted :::");
+  fetch("https://api.meaningcloud.com/sentiment-2.1", {
+    method: "POST",
+    body: {
+      key: process.env.API_KEY,
+      txt: formText,
+      lang: "en",
+    },
+  })
+    .then((res) => res.json())
+    .then(function (res) {
+      console.log("res", res);
+      document.getElementById("results").innerHTML = res.message;
+    });
 }
 
-export { handleSubmit }
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,22 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+const dotenv = require("dotenv");
 
-const app = express()
+dotenv.config();
 
-app.use(express.static('dist'))
+const app = express();
 
-console.log(__dirname)
+app.use(express.static("dist"));
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+console.log(__dirname);
+
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.listen(8081, function () {
+  console.log("Example app listening on port 8081!");
+});

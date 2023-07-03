f7efd85dad956957265a661268cfe91f116e7e60
dahhnym <danimkim.dev@gmail.com>
2023-07-04 00:59:23 +0900

    💄 add github repo link at footer

--- src/client/index.js
+++ src/client/index.js
@@ -6,5 +6,6 @@ import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
 import "./styles/form-result.scss";
+import "./../../images/github-logo.png";
 
 export { handleSubmit, resetInput };
